7a5524cb490b4e178f2aedbd313644f1869e2d0c
Ondrej Chvala <ondrejch@gmail.com>
2020-08-25 16:11:19 -0400

    dev

--- scripts/deplete_small_ag_set.py
+++ scripts/deplete_small_ag_set.py
@@ -17,13 +17,13 @@ ag_rs = np.arange(130,refl, 30)             # Positions of silver shell
 
 my_path = "/home/ondrejch/APump/MCFR/ag/small"
 
-
-mycore = msfr.MSFR(r, refl, 0.1975, "66.66%NaCl+33.34%UCl3", ag_r)
-mycore.power = 1e9      # 1 GWth
-mycore.deplete = 10     # 10 years
-mycore.qsub_file = my_path + "/run.sh"
-mycore.save_qsub_file()
-mycore.deck_path = my_path + "/run0"
-mycore.save_deck()
-print (mycore.deck_path)
-mycore.run_deck()
+for ag_r in ag_rs:
+    mycore = msfr.MSFR(r, refl, 0.1975, "66.66%NaCl+33.34%UCl3", ag_r)
+    mycore.power = 1e9      # 1 GWth
+    mycore.deplete = 10     # 10 years
+    mycore.qsub_file = my_path + "/run.sh"
+    mycore.save_qsub_file()
+    mycore.deck_path = my_path + "/ag_r-" + str(ag_r)
+    mycore.save_deck()
+    print (mycore.deck_path)
+    mycore.run_deck()
