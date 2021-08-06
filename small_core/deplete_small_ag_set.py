edbf5fcca7da58a943771e67b1e00eb63b6c155b
Ondrej Chvala <ondrejch@gmail.com>
2021-08-06 18:02:59 -0400

    set libraries and histories

--- small_core/deplete_small_ag_set.py
+++ small_core/deplete_small_ag_set.py
@@ -15,12 +15,15 @@ refl = r + relf_thickness
 ag_rs = np.arange(130,refl, 30)             # Positions of silver shell
 #ag_d = 0.05             # 0.5 mm thick silver shell - default
 
-my_path = "/home/ondrejch/APump/MCFR/ag/small"
+my_path = "/home/ondrejch/APump/MCFR/ag/small_jeff33"
 
 for ag_r in ag_rs:
     mycore = msfr.MSFR(r, refl, 0.1975, "66.66%NaCl+33.34%UCl3", ag_r)
     mycore.power = 1e9      # 1 GWth
     mycore.deplete = 10     # 10 years
+    mycore.queue = 'fill'
+    mycore.ompcores = 64
+    mycore.histories = 100000
     mycore.qsub_file = my_path + "/run.sh"
     mycore.save_qsub_file()
     mycore.deck_path = my_path + "/ag_r-" + str(ag_r)
